604d1ccca4acfbc1c22e24e410e02c233323fe8c
fwillperigon <felix.williams849@schools.sa.edu.au>
2025-08-21 11:57:56 +0930

    session

--- Felix/main.js
+++ Felix/main.js
@@ -16,8 +16,7 @@ const special_test = new RegExp("[! #$%&'()*+,-./:;<=>?@]+");
 
 function main()
 {
-    //Register();
-    Encrypt("perigon");
+    Register();
 }
 
 function CheckPassword(pass)
@@ -92,15 +91,29 @@ function Register()
         }
     }
 
-    // Encrypt password
+    var encryptedpass = Encrypt(pass);
     
-    registeredUser = new UserObject(name, pass, 0, classcode, year, null);
+    registeredUser = new UserObject(name, encryptedpass, 0, classcode, year, null);
     console.log(registeredUser);
     // add user to database
 }
 
+function Login()
+{
+    var name = prompt("Input name");
+    var pass = prompt("Input password");
 
-// work on later
+    var login_succeed = false;
+
+    while(login_succeed == false)
+    {
+        if (true)
+        {
+            
+        }
+    }
+}
+
 function Encrypt(pass)
 {
     const key = "29OJLjvs1y3GstLy";
@@ -133,23 +146,43 @@ function Encrypt(pass)
     }
 
     var returnpass = passarray.join("");    
+    return returnpass;
+}
 
-/*
-    //console.log(TempBox);
-    for (var j = 0; j < 85; j++)
+function Decrypt(pass)
+{
+    var passarray = 
+    [
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0
+    ];
+
+    for (var i = 0; i < pass.length; i++)
+    {
+        passarray[i] = pass.charCodeAt(i);
+    }
+
+    for (var j = 0; j < 10; j++)
     {
         for (var i = 0; i < 16; i++)
         {
-            TempBox[i] = SBox[InvSBox[i]];
+            passarray[i] = invsbox[passarray[i]];
         }
-        for (var i = 0; i < 16; i++)
-        {
-            TestScramble[i] = TempBox[i];
-        }
+        antiShiftRows(passarray);
     }
-*/
 
-    console.log(returnpass);
+    for (var i = 0; i < 16; i++)
+    {
+        if (String.fromCharCode(passarray[i]) != '\x00')
+            passarray[i] = String.fromCharCode(passarray[i]);
+        else
+            passarray.pop();
+    }
+
+    var returnpass = passarray.join("");    
+    return returnpass;
 }
 
 function shiftRows(arr)
@@ -166,6 +199,20 @@ function shiftRows(arr)
     swapArr(arr, 15, 14);
 }
 
+function antiShiftRows(arr)
+{
+    swapArr(arr, 4, 5);
+    swapArr(arr, 4, 6);
+    swapArr(arr, 4, 7);
+
+    swapArr(arr, 8, 10);
+    swapArr(arr, 9, 11);
+
+    swapArr(arr, 15, 14);
+    swapArr(arr, 15, 13);
+    swapArr(arr, 15, 12);
+}
+
 function swapArr(arr, a, b)
 {
     temp = arr[a];
@@ -175,12 +222,24 @@ function swapArr(arr, a, b)
 
 const sbox = 
 [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     121, 55, 93, 88, 66, 61, 53, 110, 112, 77, 126, 41, 82, 81, 62, 43, 
     69, 65, 104, 109, 70, 44, 100, 78, 40, 50, 36, 96, 114, 54, 103, 87, 
     115, 102, 111, 116, 52, 107, 117, 80, 37, 120, 39, 106, 101, 123, 68, 
     38, 85, 91, 71, 67, 95, 49, 119, 124, 99, 113, 72, 48, 56, 42, 76, 51, 
     90, 75, 98, 118, 92, 59, 105, 63, 74, 73, 97, 33, 64, 89, 122, 58, 60, 
     57, 34, 125, 108, 46, 84, 79, 86, 45, 83, 35, 94, 47
+]
+
+const invsbox = 
+[
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    108, 115, 124, 59, 73, 80, 75, 57, 44, 94, 48, 54, 122, 118, 126, 92, 
+    86, 58, 96, 69, 39, 62, 34, 93, 114, 112, 102, 113, 38, 47, 104, 109, 
+    50, 37, 84, 79, 49, 53, 83, 91, 106, 105, 98, 95, 42, 56, 120, 72, 46, 
+    45, 123, 119, 81, 121, 64, 36, 110, 97, 82, 101, 35, 125, 85, 60, 107, 
+    99, 89, 55, 77, 66, 63, 51, 103, 76, 70, 117, 52, 40, 67, 41, 90, 61, 65,
+    68, 71, 100, 87, 74, 33, 111, 78, 88, 116, 43
 ]
